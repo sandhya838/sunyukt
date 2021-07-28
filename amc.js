a08a44499cb5de7539c812ca4ee6797c7f3bf3a7
sandhya838 <78634804+sandhya838@users.noreply.github.com>
2021-07-29 01:04:07 +0530

    modefied support page
    Co-Authored-By: Virendra Gayakwad <62784876+Viren-hub@users.noreply.github.com>

---
+++ amc.js
@@ -0,0 +1,29 @@
+const amc=`<div class="amc-container">
+<!--title section -->
+<section class="elementor-element elementor-element-ff8ada1 elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section" data-id="ff8ada1" data-element_type="section">
+    <div class="elementor-widget-container-amc">
+        <h2 class="elementor-heading-title elementor-size-default">After Sales Support</h2>     </div>
+</section>
+<!-- subsection -->
+    <div class="elementor-text-editor-amc elementor-clearfix"><div class="subheading-amc"><p>Our After sales Support is equipped with below points :</p><div></div>
+    
+
+<!-- section 2nd -->
+<section class="elementor-element elementor-element-ff8ada1 elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section" data-id="ff8ada1" data-element_type="section">
+    <div class="elementor-row-amc">
+        <div class="elementor-text-editor-amc elementor-clearfix-amc">
+            <h4 class="amch4"><strong>1. Employee Training</strong></h4>
+            <p class="amcpara">After sales service does not focus solely on a product’s technical ability to function, but also on customer satisfaction. Our employees are not only prepared as technical experts ,but also customer connect is been inherited in them to learn customer-oriented thinking.</p>
+            <h4 class="amch4"><strong>2. Customer Loyalty</strong></h4>
+            <p class="amcpara">Sunyukt Electrosolar &amp; IT solutions Pvt. Ltd. that place value in good after sales management experience this loyalty emphatically. We with customers try to maintain the relationship ven when a different supplier makes a seemingly less expensive offer. Our Customer know that they have a reliable partner with their supplier, someone who is capable of carrying out repairs and providing replacement parts.</p>
+            <h4 class="amch4"><strong>3. Maintenance &amp; Service Support</strong></h4>
+            <p class="amcpara">Quick and reliable repair of technical deficiencies is a mark of quality for every business. A requirement for this, however, is that the employees are in a position to offer such after sales service through the availability of replacement parts. Frustration is preprogrammed when replacement parts are available in the capital only, which means that week’s pass by until they reach the customer out in the countryside. We as company adhere to keep Professional personnel stationed close to the customers and must have available an appropriately furnished repair workshop with replacement parts. Customers must, and want to see that they are not left alone with their technical problems.</p>
+            <h4 class="amch4"><strong>4. Customer Training</strong></h4>
+            <p class="amcpara">Many people today have expectations of solar technology that are often not possible to fulfill: a simple solar lamp is meant to not only additionally charge a mobile phone, but also supply a radio or even better yet, a small television with power. And, if possible, do so twenty-four hours a day. No one would expect that of a mobile phone, but many technical laypeople expect solar technologies to be super products. Most products used in daily life require no special explanation: their benefits and disadvantages are obvious. With solar facilities, it is different: only very few people have been able to gather experience with this technology or have observed from others, how it works. They do not know what care the product demands or when it has reached its limits. Thus, in addition to instructions for how to correctly charge the battery, customer training is also included to care for and deal with the product as a whole.</p>
+            <h4 class="amch4"><strong>5. Environment Friendly</strong></h4>
+            <p class="amcpara">We are an environmentally conscious company with continuous improvement methodologies. Our vendor selection and manufacturing processes are based on environmental protection, workplace safety and employees health. All we are working towards is a cleaner, greener and healthier future for all of us.</p><h4><strong>6. Transparency</strong></h4>
+            <p class="amcpara">For us transparency implies openness, communication and accountability towards our suppliers, employees, customer and stakeholders. Clear and precise communication forms the footboard of our openness to remove all barriers and facilitating free and easy access to all our actions, products and services.</p>
+        </div>
+    </div>
+</section>
+</div>`
